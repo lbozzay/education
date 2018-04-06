2a855f2e94a95605c0e4eb66e948294e730efc1c
Laszlo Bozzay <GB102659@Laszlos-Air.home>
2018-04-06 18:07:41 +0100

    oneVsAll in progress

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -35,31 +35,31 @@ grad = zeros(size(theta));
 %           temp(1) = 0;   % because we don't add anything for j = 0
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
-  fprintf("size X: %f \n", size(X));
-  fprintf("size theta: %f \n", size(theta));
-  fprintf("\n");
-  fprintf("theta: %f\n", theta);
-  fprintf("thetaT: %f", theta');
+%  fprintf("size X: %f \n", size(X));
+%  fprintf("size theta: %f \n", size(theta));
+%  fprintf("\n");
+%  fprintf("theta: %f\n", theta);
+%  fprintf("thetaT: %f", theta');
   h = sigmoid(X * theta);
-  fprintf("\n");
-  fprintf("h: %f\n", h);
+%  fprintf("\n");
+%  fprintf("h: %f\n", h);
   J_p = (-y .* log(h))-((1-y) .* log(1-h));
-  fprintf("J_p: %f\n", J_p);
+%  fprintf("J_p: %f\n", J_p);
   J_nonreg = 1/m .* sum(J_p);
-  fprintf("J_nonreg: %f\n", J_nonreg);
+%  fprintf("J_nonreg: %f\n", J_nonreg);
 
   J_reg = lambda / (2 * m) .* sum(theta(2:end).^2);
-  fprintf("J_reg: %f\n", J_reg);
+%  fprintf("J_reg: %f\n", J_reg);
 
   J = J_nonreg + J_reg;
-  fprintf("J: %f\n", J);
+%  fprintf("J: %f\n", J);
 
   grad_non_reg = 1/m .* (X' * (h-y));
-  fprintf("grad_non_reg: %f\n", grad_non_reg);
-  fprintf("grad_non_reg size: %f\n", size(grad_non_reg));
+%  fprintf("grad_non_reg: %f\n", grad_non_reg);
+%  fprintf("grad_non_reg size: %f\n", size(grad_non_reg));
   grad_reg = grad_non_reg + ((lambda / m) * sum(theta(2:end).^2));
-  fprintf("grad_reg: %f\n", grad_reg);
-  fprintf("grad_reg size: %f\n", size(grad_reg));
+%  fprintf("grad_reg: %f\n", grad_reg);
+%  fprintf("grad_reg size: %f\n", size(grad_reg));
 
   grad(1) = grad_non_reg(1);
   grad(2:end) = grad_non_reg(2:end) + ((lambda / m) * (theta(2:end)));
